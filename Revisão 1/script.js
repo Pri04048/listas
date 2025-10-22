fdb92a7176e1db3985dcdaca72732c5adce6e4dc
Pri04048 <priscila.s.silva12@aluno.senai.br>
2025-10-22 09:10:16 -0300

    revisao

---
+++ Revisão 1/script.js
@@ -0,0 +1,55 @@
+let numero1 = Number(prompt("Informe um número"))
+if(numero1 >= 10 && numero1 <= 20) {
+    console.log("Está entre 10 e 20")
+}    // tudo tem que ser verdade
+
+let numero2 = Number(prompt("Informe um número"))
+if (numero2 == 10 || numero2) {}
+let nome = "Priscila"
+let idade = 17
+let valor = prompt("Digite um valor: ")
+
+console.log("meu nome é", nome, "e tenho", idade, "anos")
+console.log(valor)
+
+let num1 = 3
+let num2 = 2
+
+let soma = num1 = num2
+let subtracao = num1 - num2
+let multiplicacao = num1 * num2
+let divisao = num1 / num2
+let resto = num1 % num2
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(resto)
+
+let valor1 = 10
+let valor2 = 5
+
+console.log(valor1 > valor2)
+console.log(valor1 >= valor2)
+console.log(valor1 < valor2)
+console.log(valor1 <= valor2)
+console.log(valor1 == valor2)
+console.log(valor1 != valor2)
+
+let idade1 = Number(prompt ("Qual sua idade"))
+if(idade1 >= 18) {
+    console.log("voce é maior de idade")
+} else {
+    console.log("voce e menor de idade")
+}
+
+let numero = Number(prompt("Digite um numero:"))
+
+let alunoSesi = true 
+let ensinoMedio = true
+if (alunoSesi && ensinoMedio) {
+    console.log("pode fazer o tecnico")
+} else {
+    console.log ("nao pode fazer o tecnico")
+}
